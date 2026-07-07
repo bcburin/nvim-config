c07e91962631d2aea4ce80b725b1c95abe82ba96
Bruno Burin <bcburin@inoa.com.br>
2026-07-07 10:32:33 -0300

    feat: udpate telescope keybindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,14 @@ return {
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     config = function()
+        local telescope = require("telescope")
         local builtin = require("telescope.builtin")
+
+        telescope.setup({
+            defaults = {
+                path_display = { "filename_first" },
+            }
+        })
 
         vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
@@ -18,13 +25,16 @@ return {
         vim.keymap.set("n", "<leader>Gs", builtin.git_status, { desc = "List Git Status" })
         vim.keymap.set("n", "<leader>GS", builtin.git_stash, { desc = "List Git Stash" })
 
-        vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, { desc = "Go to Definitions" })
-        vim.keymap.set("n", "<leader>gr", builtin.lsp_references, { desc = "Go to References" })
-        vim.keymap.set("n", "<leader>gI", builtin.lsp_implementations, { desc = "Go to Implementations" })
-        vim.keymap.set("n", "<leader>gT", builtin.lsp_type_definitions, { desc = "Go to Type Definitions" })
-        vim.keymap.set("n", "<leader>gw", builtin.lsp_document_symbols, { desc = "List Document Symbols in Current Workspace" })
-        vim.keymap.set("n", "<leader>gW", builtin.lsp_dynamic_workspace_symbols, { desc = "List Document Symbols for All Workspaces" })
-        vim.keymap.set("n", "<leader>gD", builtin.diagnostics, { desc = "List Diagnostics" })
+        vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions, { desc = "Go to Definitions" })
+        vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Go to References" })
+        vim.keymap.set("n", "<leader>fI", builtin.lsp_implementations, { desc = "Go to Implementations" })
+        vim.keymap.set("n", "<leader>fT", builtin.lsp_type_definitions, { desc = "Go to Type Definitions" })
+        vim.keymap.set("n", "<leader>fw", builtin.lsp_document_symbols, { desc = "List Document Symbols in Current Workspace" })
+        vim.keymap.set("n", "<leader>fW", builtin.lsp_dynamic_workspace_symbols, { desc = "List Document Symbols for All Workspaces" })
+        vim.keymap.set("n", "<leader>fD", builtin.diagnostics, { desc = "List Diagnostics" })
+
+        -- Find Neovim configuration files
+        vim.keymap.set("n", "<leader>fc", function() builtin.find_files({ cwd = vim.fn.stdpath("config") }) end, { desc = "Find Neovim Configuration Files" })
 
         require("telescope").load_extension("jsonfly")
         vim.api.nvim_create_autocmd("FileType", {
