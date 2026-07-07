a3e449c6602d81e2c60c6a1cf679c12b4dbe2817
Bruno Burin <bcburin@inoa.com.br>
2026-07-07 12:40:18 -0300

    feat: install vim-fugitive

---
+++ lua/plugins/vim-fugitive.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+        "G",
+        "Git",
+        "Gdiffsplit",
+        "Gvdiffsplit",
+        "Gread",
+        "Gwrite",
+    },
+  }
+}
